3f35e0fb83236151f00cdf79114e9a6419c729e4
Denis <nisetadenis@gmail.com>
2024-06-25 15:58:29 +0300

    Fix cookies

--- JS/Homework№11(Webstorage)/assets/scripts/script.js
+++ JS/Homework№11(Webstorage)/assets/scripts/script.js
@@ -1,4 +1,5 @@
-let btnSearch = document.querySelector(".weather__search")
+import "../utils/cookies.js"
+import { getCookie, setCookie } from "../utils/cookies.js"
 
 let cityBox = document.querySelector(".weather__info__city")
 let tempBox = document.querySelector(".weather__info__temp")
@@ -6,85 +7,87 @@ let infoBox = document.querySelector(".weather__info__more-info")
 
 async function log() {
 
-    let inputCity = document.getElementById("city").value
-
     const APIKEY = '7123d0186166e5633adeda90dc01cd15'
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${APIKEY}&lang=ua`
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=Poltava&appid=${APIKEY}&lang=ua`
+    
     let response = await fetch(API)
     let result = await response.json()
-    return result
+
+    let cachedReg = !!getCookie("weather")
+    let cookieWeather;
+
+    if(cachedReg) {
+        cookieWeather = getCookie("weather")
+        return JSON.parse(cookieWeather)
+    } else {
+        cookieWeather = setCookie("weather",JSON.stringify(result))
+        return log()
+    }
+
 }
+
+setTimeout(() => {
+    let data = log()
+    displayWeather(data)
+},800)
 
 async function displayWeather(objInfo) {
-    try {
 
-        let result = await objInfo
+    let result = await objInfo
     
-        // Result of temperaturs
-        let tempMain = (result.main.temp - 273.15).toFixed(1) + "℃"
-        let tempMax = (result.main.temp_max - 273.15).toFixed(1) + "℃"
-        let tempMin = (result.main.temp_min - 273.15).toFixed(1) + "℃"
-        let tempFeel = (result.main.feels_like - 273.15).toFixed(1) + "℃"
+    // Result of temperaturs
+    let tempMain = (result.main.temp - 273.15).toFixed(1) + "℃"
+    let tempMax = (result.main.temp_max - 273.15).toFixed(1) + "℃"
+    let tempMin = (result.main.temp_min - 273.15).toFixed(1) + "℃"
+    let tempFeel = (result.main.feels_like - 273.15).toFixed(1) + "℃"
     
-        let humidity = result.main.humidity + "%"
-        let seaLevel = result.main.sea_level + " гПа"
-        let groundLevel = result.main.grnd_level + " гПа"
+    let humidity = result.main.humidity + "%"
+    let seaLevel = result.main.sea_level + " гПа"
+    let groundLevel = result.main.grnd_level + " гПа"
     
-        let windSpeed = result.wind.speed + " m/s"
-        cityBox.innerHTML = `
-            <p class="city">City name: ${result.name}</p>
-            <p class="country">Country: ${result.sys.country}</p>
-            <p class="coords">lon: ${result.coord.lon}  lat: ${result.coord.lat}</p>
-        `
+    let windSpeed = result.wind.speed + " m/s"
+    cityBox.innerHTML = `
+        <p class="city">City name: ${result.name}</p>
+        <p class="country">Country: ${result.sys.country}</p>
+        <p class="coords">lon: ${result.coord.lon}  lat: ${result.coord.lat}</p>
+    `
     
-        tempBox.innerHTML = `
-            <p class="max-main">Main temperature: ${tempMain}</p>
-            <p class="fell-temp">Feels like: ${tempFeel}</p>
-            <p class="max-temp">Max: ${tempMax}</p>
-            <p class="min-temp">Min: ${tempMin}</p>
-        `
+    tempBox.innerHTML = `
+        <p class="max-main">Main temperature: ${tempMain}</p>
+        <p class="fell-temp">Feels like: ${tempFeel}</p>
+        <p class="max-temp">Max: ${tempMax}</p>
+        <p class="min-temp">Min: ${tempMin}</p>
+    `
     
-        infoBox.innerHTML = `
-            <p class="humidity">Humidity: ${humidity}</p>
-            <p class="sea-level">Sea level: ${seaLevel}</p>
-            <p class="ground-level">Ground level: ${groundLevel}</p>
-            <p class="wind-speed">Wind speed: ${windSpeed}</p>
-        `
-
-    } catch {
-        let notFound = document.querySelector(".not-found")
-        notFound.style.bottom = "13%"
-        setTimeout(() => {
-            notFound.style.bottom = "22%"
-        },2000)
-    }
+    infoBox.innerHTML = `
+        <p class="humidity">Humidity: ${humidity}</p>
+        <p class="sea-level">Sea level: ${seaLevel}</p>
+        <p class="ground-level">Ground level: ${groundLevel}</p>
+        <p class="wind-speed">Wind speed: ${windSpeed}</p>
+    `
 }
 
-btnSearch.addEventListener("click", () => {
-    let data = log()
-    cityBox.innerHTML = `
-         <div class="animation">
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-        </div>
-    `
-    tempBox.innerHTML = `
-         <div class="animation">
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-        </div>
-    `
-    infoBox.innerHTML = `
-         <div class="animation">
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-            <span class="animation__cube"></span>
-        </div>
-    `
-    setTimeout(() => {
-        displayWeather(data)
-    },800)
+cityBox.innerHTML = `
+     <div class="animation">
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+    </div>
+`
 
-})
+tempBox.innerHTML = `
+     <div class="animation">
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+    </div>
+`
+
+infoBox.innerHTML = `
+     <div class="animation">
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+        <span class="animation__cube"></span>
+    </div>
+`
+
---
+++ JS/Homework№11(Webstorage)/assets/utils/cookies.js
@@ -0,0 +1,11 @@
+export const setCookie = (name, value) => {
+    document.cookie = `${name}=${value};max-age=7200`
+}
+
+
+export function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
