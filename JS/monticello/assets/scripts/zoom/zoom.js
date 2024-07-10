a11fa7b685463d79ff4a6863553c9e345c659806
Денис <161925751+DenisNysheta@users.noreply.github.com>
2024-07-10 10:11:12 +0300

    Fix listener for zoom in gallery

--- JS/monticello/assets/scripts/zoom/zoom.js
+++ JS/monticello/assets/scripts/zoom/zoom.js
@@ -27,6 +27,30 @@ galleryBox.addEventListener("click", (e) => {
         let gallary = document.querySelector(".gallery")
         gallary.appendChild(boxForView)
 
+    } else if (e.target.tagName == "svg") {
+        let boxForView = document.createElement("div")
+        boxForView.style.width = "100vw"
+        boxForView.style.height = "112vh"
+        boxForView.style.opacity = "1"
+        boxForView.style.position = "absolute"
+        boxForView.style.top = "0"
+        boxForView.style.left = "0"
+        boxForView.classList.add("zoom")
+        // console.log(e.target.parentElement.nextElementSibling.src)
+        let srcForImg = e.target.parentElement.nextElementSibling.src
+
+        boxForView.innerHTML = `
+        <div class="zoom-wrapper">
+            <div class="zoom-background"></div>
+            <div class="back-img">
+                <img style="width:600px" src="${srcForImg}" alt="" class="zoom-image"/>
+            </div>
+            <button class="zoom-btn">Close</button>
+        </div>
+    `
+
+    let gallary = document.querySelector(".gallery")
+    gallary.appendChild(boxForView)
     }
 })
 
